22f5b73544f7157d2b1992570b4ab9d56b207a87
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2023-04-05 21:14:07 +0200

    added library, video player and feedback form

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,45 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+const galleryListCard = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item" style= "border-radius: 4%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
+  <a class="gallery__link" href="${original}" style= "border-radius: 4%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      style= "border-radius: 4%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', galleryListCard);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
 console.log(galleryItems);
